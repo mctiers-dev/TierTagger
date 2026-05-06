bb7b4ec09c7047e2ae2f6fc7ffb3bddedaeda49d
Lyosha <lyosha@lyosha.sh>
2026-05-05 20:05:07 -0400

    fix: blah blah blah u cant make me write it

--- common/src/main/java/net/uku3lig/tiertagger/TierTagger.java
+++ common/src/main/java/net/uku3lig/tiertagger/TierTagger.java
@@ -73,8 +73,141 @@ public class TierTagger {
     }
 
     public static Component appendTier(UUID uuid, Component text) {
+        return appendTier(uuid, text, text);
+    }
+
+    public static Component appendTier(UUID uuid, Component text, Component plainName) {
+        Component base = KitDetector.isOnMcpvp() ? plainName : text;
+
         MutableComponent following = getPlayerTier(uuid)
                 .map(entry -> {
+                    Component tierText = getRankingText(entry.ranking(), false);
+
+                    if (manager.getConfig().isShowIcons() && entry.mode() != null && entry.mode().icon().isPresent()) {
+                        return Component.literal(entry.mode().icon().get().toString()).append(tierText);
+                    } else {
+                        return tierText.copy();
+                    }
+                })
+                .orElse(null);
+
+        if (following != null) {
+            following.append(Component.literal(" | ").withStyle(ChatFormatting.GRAY));
+            return following.append(base);
+        }
+
+        return base;
+    }
+
+    public static Optional<PlayerInfo.NamedRanking> getPlayerTier(UUID uuid) {
+        GameMode detected = KitDetector.currentGameMode();
+        GameMode mode = detected != GameMode.NONE ? detected : manager.getConfig().getGameMode();
+
+        return TierCache.getPlayerRankings(uuid)
+                .map(rankings -> {
+                    PlayerInfo.Ranking ranking = rankings.get(mode.id());
+                    if (detected != GameMode.NONE){
+                        if (ranking == null) {
+                            return null;
+                        } else {
+                            return ranking.asNamed(mode);
+                        }
+                    }
+                    Optional<PlayerInfo.NamedRanking> highest = PlayerInfo.getHighestRanking(rankings);
+                    TierTaggerConfig.HighestMode highestMode = manager.getConfig().getHighestMode();
+                    if (ranking == null) {
+                        if (highestMode != TierTaggerConfig.HighestMode.NEVER && highest.isPresent()) {
+                            return highest.get();
+                        } else {
+                            return null;
+                        }
+                    } else {
+                        if (highestMode == TierTaggerConfig.HighestMode.ALWAYS && highest.isPresent()) {
+                            return highest.get();
+                        } else {
+                            return ranking.asNamed(mode);
+                        }
+                    }
+                });
+    }
+
+    private static MutableComponent getTierText(int tier, int pos, boolean retired) {
+        StringBuilder text = new StringBuilder();
+        if (retired) text.append("R");
+        text.append(pos == 0 ? "H" : "L").append("T").append(tier);
+
+        int color = TierTagger.getTierColor(text.toString());
+        return Component.literal(text.toString()).withStyle(s -> s.withColor(color));
+    }
+
+    public static Component getRankingText(PlayerInfo.Ranking ranking, boolean showPeak) {
+        if (ranking.retired() && ranking.peakTier() != null && ranking.peakPos() != null) {
+            return getTierText(ranking.peakTier(), ranking.peakPos(), true);
+        } else {
+            MutableComponent tierText = getTierText(ranking.tier(), ranking.pos(), false);
+
+            if (showPeak && ranking.comparablePeak() < ranking.comparableTier()) {
+                // warning caused by potential NPE by unboxing of peak{Tier,Pos} which CANNOT happen, see impl of comparablePeak
+                // noinspection DataFlowIssue
+                tierText.append(Component.literal(" (peak: ").withStyle(s -> s.withColor(ChatFormatting.GRAY)))
+                        .append(getTierText(ranking.peakTier(), ranking.peakPos(), false))
+                        .append(Component.literal(")").withStyle(s -> s.withColor(ChatFormatting.GRAY)));
+            }
+
+            return tierText;
+        }
+    }
+
+    public static int getTierColor(String tier) {
+        if (tier.startsWith("R")) {
+            return manager.getConfig().getRetiredColor();
+        } else {
+            return manager.getConfig().getTierColors().getOrDefault(tier, 0xD3D3D3);
+        }
+    }
+
+    private static void checkForUpdates() {
+        String versionParam = "[\"%s\"]".formatted(SharedConstants.getCurrentVersion().name());
+        String fullUrl = UPDATE_URL_FORMAT.formatted(URLEncoder.encode(versionParam, StandardCharsets.UTF_8));
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create(fullUrl)).GET().build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(r -> {
+                    String body = r.body();
+                    JsonArray array = GSON.fromJson(body, JsonArray.class);
+
+                    if (!array.isEmpty()) {
+                        JsonObject root = array.get(0).getAsJsonObject();
+
+                        String versionName = root.get("name").getAsString();
+                        if (versionName != null && versionName.toLowerCase(Locale.ROOT).startsWith("[o")) {
+                            isObsolete.set(true);
+                        }
+
+                        String latestVer = root.get("version_number").getAsString();
+                        try {
+                            return Version.parse(latestVer);
+                        } catch (ParseException e) {
+                            logger.warn("Could not parse version number {}", latestVer);
+                        }
+                    }
+
+                    return null;
+                })
+                .exceptionally(t -> {
+                    logger.warn("Error checking for updates", t);
+                    return null;
+                }).thenAccept(v -> {
+                    logger.info("Found latest version {}", v);
+                    latestVersion = v;
+                });
+    }
+
+    public static boolean isObsolete() {
+        return isObsolete.get();
+    }
+}                .map(entry -> {
                     Component tierText = getRankingText(entry.ranking(), false);
 
                     if (manager.getConfig().isShowIcons() && entry.mode() != null && entry.mode().icon().isPresent()) {
